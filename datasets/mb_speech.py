821ba50357d7f061f32ed39590a602ea316a321b
Hannes1 <39419636+Hannes1@users.noreply.github.com>
2020-11-01 12:20:30 +0200

    Update mb_speech.py

--- datasets/mb_speech.py
+++ datasets/mb_speech.py
@@ -7,11 +7,13 @@ import numpy as np
 
 from torch.utils.data import Dataset
 
-vocab = "B абвгдеёжзийклмноөпрстуүфхцчшъыьэюя"  # B: blank
+#Going to have to create my own vocab for english
+vocab = "B абвгдеёжзийклмноөпрстуүфхцчшъыьэюя"  # B: blank  #vocab gets imported seperatly
 char2idx = {char: idx for idx, char in enumerate(vocab)}
 idx2char = {idx: char for idx, char in enumerate(vocab)}
 
 
+#here we return the audios text used ensuring only lowercase characters and valid characters
 def convert_text(text):
     text = text.lower()
     # ignore all characters which is not in the vocabulary
@@ -33,12 +35,12 @@ def read_metadata(dataset_path, max_duration):
         except ValueError:
             continue
 
-        fnames.append(os.path.join(dataset_path, 'wavs', '%s.wav' % fname))
-        texts.append(np.array(convert_text(text)))
+        fnames.append(os.path.join(dataset_path, 'wavs', '%s.wav' % fname)) #get path to audio wav
+        texts.append(np.array(convert_text(text))) #get text to audio
 
     return fnames, texts
 
-
+#gets called and reads to csv file and return the text and file path
 class MBSpeech(Dataset):
 
     def __init__(self, max_duration=16.7, transform=None):
@@ -55,7 +57,7 @@ class MBSpeech(Dataset):
         }
 
         if self.transform is not None:
-            data = self.transform(data)
+            data = self.transform(data) #tranform is passed as param in train.py 
 
         return data
 
